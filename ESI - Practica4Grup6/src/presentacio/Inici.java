34650f6ddeb61405d3d8a694036a3264c30c1c83
santi herranz <santi.herranz@gmail.com>
2015-11-25 00:32:55 +0100

    Canvi aliniació components Tecla enter per acceptar

--- ESI - Practica4Grup6/src/presentacio/Inici.java
+++ ESI - Practica4Grup6/src/presentacio/Inici.java
@@ -11,6 +11,8 @@ import java.awt.BorderLayout;
 import javax.swing.AbstractAction;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.Action;
 import javax.swing.JTextField;
@@ -25,6 +27,7 @@ import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.Component;
 import javax.swing.Box;
+import java.awt.Dimension;
 
 public class Inici {
 
@@ -60,50 +63,73 @@ public class Inici {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setTitle("Practica 4 - Grup 6");
-		frame.setBounds(100, 100, 329, 202);
+		frame.setBounds(100, 100, 329, 185);
 		frame.setLocationRelativeTo(null); // center the application window
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		frame.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
 		Box verticalBox = Box.createVerticalBox();
+		verticalBox.setAlignmentX(Component.CENTER_ALIGNMENT);
 		frame.getContentPane().add(verticalBox);
 		
+		Component verticalStrut = Box.createVerticalStrut(20);
+		verticalStrut.setPreferredSize(new Dimension(0, 50));
+		verticalBox.add(verticalStrut);
+		
+		Box horizontalBox = Box.createHorizontalBox();
+		verticalBox.add(horizontalBox);
+		
 		JLabel lblMida = new JLabel("Mida:");
-		verticalBox.add(lblMida);
+		horizontalBox.add(lblMida);
+		lblMida.setAlignmentX(Component.CENTER_ALIGNMENT);
 		
 		textField = new JTextField();
-		verticalBox.add(textField);
+		horizontalBox.add(textField);
 		textField.setColumns(10);
+		textField.addKeyListener(new KeyAdapter(){
+			
+			public void keyPressed(KeyEvent e) {
+		           int key = e.getKeyCode();
+		           if (key == KeyEvent.VK_ENTER) {
+		        	   iniciarJoc();
+		              }
+		         }			
+			
+		});
 		
 		JButton btnAcceptar = new JButton("Acceptar");
-		verticalBox.add(btnAcceptar);
+		horizontalBox.add(btnAcceptar);
+		btnAcceptar.setAlignmentX(Component.CENTER_ALIGNMENT);
 		btnAcceptar.addActionListener(new ActionListener(){
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				
-				String text = textField.getText();
-				
-				try {
-
-					int value = Integer.parseInt(text);
-					Joc joc = new Joc(value);
-
-					Aplicacio app = new Aplicacio(joc);
-					frame.setVisible(false);
-					
-				} catch (Exception e) {
-					JOptionPane.showMessageDialog(frame,
-						    e.getMessage(),
-						    "error",
-						    JOptionPane.ERROR_MESSAGE);
-				}
-				
-				
+				iniciarJoc();
 			}});
 		
 		
 	}
 
+	
+	private void iniciarJoc(){
+		String text = textField.getText();
+		
+		try {
+
+			int value = Integer.parseInt(text);
+			Joc joc = new Joc(value);
+
+			Aplicacio app = new Aplicacio(joc);
+			frame.setVisible(false);
+			
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(frame,
+				    e.getMessage(),
+				    "error",
+				    JOptionPane.ERROR_MESSAGE);
+			 textField.setText("");
+			 textField.requestFocus();
+		}		
+	}
 }
