35749256905c985fb6bce8ab5ed7089ecea4c7ea
santi herranz <santi.herranz@gmail.com>
2015-11-29 20:01:21 +0100

    Revisar amb el professor

--- ESI - Practica4Grup6/src/domini/Coord.java
+++ ESI - Practica4Grup6/src/domini/Coord.java
@@ -1,6 +1,12 @@
 package domini;
 
 public class Coord {
-		public int x; public int y;
-		Coord(int x,int y){ this.x = x; this.y=y;};
+		public int x; 
+		public int y;
+		Coord(int x,int y){ 
+			this.x = x; 
+			this.y=y;
+		};
+		
 }
+
